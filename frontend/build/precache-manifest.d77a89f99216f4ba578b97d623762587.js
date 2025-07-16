7b362e96e3fa2d377f5e7623d1acbe47f8546eba
D. M. Khalid Mahmud <134827690+dm-khalid@users.noreply.github.com>
2025-07-16 18:29:21 +0600

    Add files via upload

---
+++ frontend/build/precache-manifest.d77a89f99216f4ba578b97d623762587.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "806e3ca22cf7a09accb9d6dc5104dd03",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c7f78c31db8cda3f67cd",
+    "url": "/static/css/main.b6ab9261.chunk.css"
+  },
+  {
+    "revision": "250f7d7e50a799179d07",
+    "url": "/static/js/2.d00de851.chunk.js"
+  },
+  {
+    "revision": "59738b10c102cc3bbc5d37ca681942b9",
+    "url": "/static/js/2.d00de851.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d9ecca4e20748bb8e03",
+    "url": "/static/js/3.d80cd0a2.chunk.js"
+  },
+  {
+    "revision": "c7f78c31db8cda3f67cd",
+    "url": "/static/js/main.3c4ac87e.chunk.js"
+  },
+  {
+    "revision": "e36ef7469f1e3d596630",
+    "url": "/static/js/runtime-main.416aa191.js"
+  },
+  {
+    "revision": "00778ded727ff8a5b66ed5e78285bf72",
+    "url": "/static/media/bg.00778ded.png"
+  },
+  {
+    "revision": "4464552dcc965ffa6eaa103c89a65f4f",
+    "url": "/static/media/logo.4464552d.png"
+  }
+]);
